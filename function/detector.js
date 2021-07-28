389f7e369afc8349620da54e475022f4dd4c0959
ParvinEyvazov <parvineyvazov@teknodev.biz>
2021-07-28 19:00:22 +0300

    dependecies updated & new trigger added

--- function/detector.js
+++ function/detector.js
@@ -1,6 +1,5 @@
 import * as Bucket from "@spica-devkit/bucket";
 const fetch = require("node-fetch");
-var Promise = require("promise");
 
 const PUBLIC_URL = process.env.__INTERNAL__SPICA__PUBLIC_URL__;
 const AUTH_APIKEY = process.env.AUTH_APIKEY;
